b58dbcace4be68f2423ac68876341d1aecde519f
Michael Hallberg <michael.hallberg@snowsoftware.com>
2017-03-28 12:08:24 +0200

    vuefire

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+  <div>
+    
   </div>
 </template>
+ 
 
 <script>
+import firebase from './firebase'
+var config = {
+  apiKey: "AIzaSyBdrml5SQC6sGW1AeAEMxFnlXvX84izaPg",
+  authDomain: "islandinventory-98ed2.firebaseapp.com",
+  databaseURL: "https://islandinventory-98ed2.firebaseio.com",
+  storageBucket: "islandinventory-98ed2.appspot.com",
+  messagingSenderId: "338111248721"
+};
+
+console.log(firebase.initializeApp(config));
+var itemRef = firebase.database().ref("test");
+console.log(itemRef);
+
 export default {
-  name: 'app'
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Test from '@/components/Test'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -9,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
-    },
-    {
-      path: '/test',
-      name: 'Test',
-      component: Test
+      name: 'Login',
+      component: Login
     }
   ]
 })
