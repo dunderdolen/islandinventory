c1e3fec9edd9c94f35f017d183592b9b43cf38fc
Michael Hallberg <michaelhallberg@dexter.local>
2017-04-19 22:59:49 +0200

    loginpage

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,102 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png"/>
-
-    <!-- v-model is used for TWO way binding. v-bind is one way binding -->
-    <form id="login-form" v-if="!isAuthorized" v-on:submit.prevent="signIn">
-        <b-form-input v-model="email" type="email" placeholder="Email"></b-form-input>
-        <b-form-input v-model="password" type="password" placeholder="Password"></b-form-input>
-        <b-button size="sm" variant="primary" type="submit" v-bind:buttonMsg='buttonMsg' v-on:click="signIn">{{ buttonMsg }}</b-button>
-    </form>
-    <div v-if="isAuthorized">
-        <Welcome></Welcome>
-        <b-button size="sm" variant="primary" v-on:click="signOut" v-bind:buttonMsg='buttonMsg'>{{ buttonMsg }}</b-button>
+    <div class="wrapper">
+        <img id="logo" src="http://placehold.it/250x150"/>
+        <!-- v-model is used for TWO way binding. v-bind is one way binding -->
+        <h2 id="welcome-text">Logga in med din email och lösenord</h2>
+        <form class="two-by-two" id="login-form" v-if="!isAuthorized" v-on:submit.prevent="signIn">
+            <input v-model="email" type="email" placeholder="Email"></input>
+            <input v-model="password" type="password" placeholder="Password"></input>
+            <button type="submit" v-bind:buttonMsg='buttonMsg' v-on:click="signIn">{{ buttonMsg }}</button>
+        </form>
+        <div v-if="isAuthorized">
+            <Welcome></Welcome>
+            <button v-on:click="signOut" v-bind:buttonMsg='buttonMsg'>{{ buttonMsg }}</button>
+        </div>
     </div>
-  </div>  
 </template>
 
 <script>
-import Firebase from 'firebase'
-import Router from '@/router'
+    import Firebase from 'firebase'
 
-var config = {
-    apiKey: "AIzaSyBdrml5SQC6sGW1AeAEMxFnlXvX84izaPg",
-    authDomain: "islandinventory-98ed2.firebaseapp.com",
-    databaseURL: "https://islandinventory-98ed2.firebaseio.com"
-}
+    var config = {
+        apiKey: "AIzaSyBdrml5SQC6sGW1AeAEMxFnlXvX84izaPg",
+        authDomain: "islandinventory-98ed2.firebaseapp.com",
+        databaseURL: "https://islandinventory-98ed2.firebaseio.com"
+    }
 
-Firebase.initializeApp(config)
-var taskRef = Firebase.database().ref('Task');
+    Firebase.initializeApp(config)
+    var taskRef = Firebase.database().ref('Task');
 
-export default {
-    name: 'app',
-    firebase: {
-        tasks: taskRef
-     },
-    data () {
-        return {
-            email: '',
-            password: '',
-            buttonMsg: '',
-            isAuthorized: false
-        }
-    },
-    mounted(){
-        var self = this;
-        Firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                self.isAuthorized = true;
-                self.buttonMsg = "Logga ut";
-                console.log("User is logged in ");                
+    export default {
+        name: 'app',
+        firebase: {
+            tasks: taskRef
+        },
+        data () {
+            return {
+                email: '',
+                password: '',
+                buttonMsg: '',
+                isAuthorized: false
             }
-            else {
-                //Stay on login
-                self.buttonMsg = "Logga in"; 
-                console.log("User is not logged in");
-            }
-        });
-    },
-    methods: {        
-        signIn () {
-            Firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                console.log(errorCode, errorMessage)
+        },
+        mounted(){
+            var self = this;
+            Firebase.auth().onAuthStateChanged(function(user) {
+                if (user) {
+                    self.isAuthorized = true;
+                    self.buttonMsg = "Logga ut";
+                    console.log("User is logged in ");                
+                }
+                else {
+                    //Stay on login
+                    self.buttonMsg = "Logga in"; 
+                    console.log("User is not logged in");
+                }
             });
         },
-        signOut() {
-            Firebase.auth().signOut();
-            this.isAuthorized = false;
-        },
+        methods: {        
+            signIn () {
+                Firebase.auth().signInWithEmailAndPassword(this.email, this.password).catch(function(error) {
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    console.log(errorCode, errorMessage)
+                });
+            },
+            signOut() {
+                Firebase.auth().signOut();
+                this.isAuthorized = false;
+            },
+        }
     }
-}
 </script>
 
 <style>
+.wrapper {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+    grid-auto-rows: minmax(100px, auto);
+}
 
+#logo { grid-column: 2 / 3; }
+#welcome-text {
+    grid-column: 2 / 3;
+    grid-row: 2;
+}
+.two-by-two {
+    grid-column: 2;
+    grid-row: 3;
+    grid-gap: 2px;
+}
+
+#login-form input, button {
+    padding: 10px;
+    margin-bottom: 2px;
+    text-align: center;
+    width: 100%;
+    font-size: 16pt;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,18 +5,11 @@ import VueFire from 'vuefire'
 import Firebase from 'firebase'
 import App from './App'
 import Welcome from './components/welcome'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 //Register components globally
-Vue.use(BootstrapVue)
 Vue.use(VueFire)
+Vue.component('Welcome', Welcome);
 
-//Components registered as below can be used 
-Vue.component('welcome', Welcome);
-
-//Creating a new vue and connect the el property to the index.html app
 new Vue({
   el: '#app',
   router,
