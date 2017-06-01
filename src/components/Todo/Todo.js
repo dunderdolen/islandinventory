7e7d99233ef07f515b53a3f37c8a337ca6b25d6c
Michael Hallberg <michael_hallberg@live.se>
2017-06-01 23:05:44 +0200

    ffwfwef

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,33 +1,15 @@
 import db from '../../db'
+import {bus} from '../../services/bus'
 var todosRef = db.ref("todos/")
-var date = new Date().toISOString().slice(0,10);
+
 export default {
   name: 'Todo',
-  firebase: {
-    todos: db.ref("todos/")
-  },
   data () {
     return {
-      title: '',
-      createdBy: '',
-      createdDate: date,
-      isEditing: false,
-      isCreating: false
+      isEditing: false
     }
   },
   methods: {
-    openForm() {
-      this.isCreating = true,
-      this.$modal.show('create-todo')
-    },
-    createTodo () {
-      this.isCreating = false
-        todosRef.push({
-        title: this.title,
-        date: this.createdDate
-      });
-      this.$modal.hide('create-todo');
-    },
     updateTodo(todo) {
       this.isEditing = true;
       todosRef.child(todo['.key']).child('title').set(todo, $event.target.value)
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,22 +1,39 @@
 import db from '../../db'
-var todosRef = db.ref("todos/")
 import Todo from '../Todo/Todo.vue'
-import Longpress from 'vue-longpress'
+import {bus} from '../../services/bus'
+var todosRef = db.ref("todos/")
+var date = new Date().toISOString().slice(0,10);
+
 export default {
   name: 'TodoList',
   props: ['isAuthorized'],
-  components: {Todo, Longpress},
+  components: {Todo},
   firebase: {
     todos: db.ref("todos/")
   },
   data () {
     return {
-      
+      isCreating: false,
+      createdDate: date,
+      title: '',
+      show: false
     }
   },
-  methods: {
-    removeTodo(todo) {
-      todosRef.child(todo).remove()  
+  methods:{
+    openForm() {
+      this.isCreating = true,
+      this.$modal.show('create-todo')
+    },
+    createTodo(todo) {
+      this.isCreating = false
+      todosRef.push({
+        title: this.title,
+        date: this.createdDate
+      });
+      this.$modal.hide('create-todo');
+    },
+    deleteTodo(todo) {
+      todosRef.child(todo['.key']).remove()   
     }
   }
 }
